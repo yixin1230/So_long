46a9f28e6f2da9defdba73da28bf4498ec4c0672
Yixin Zhang <yizhang@f0r1s7.codam.nl>
2023-01-31 08:09:08 +0100

    ok

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: yizhang <yizhang@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/01/25 18:33:30 by yizhang       #+#    #+#                 */
-/*   Updated: 2023/01/27 17:05:55 by yizhang       ########   odam.nl         */
+/*   Updated: 2023/01/27 18:51:31 by yizhang       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,3 +17,12 @@
 #include <memory.h>
 
 
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world");
+	mlx_loop(mlx);
+}
